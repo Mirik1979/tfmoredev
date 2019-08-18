50dacd66b3981d8389e7121ee8c8e1eae88147f1
miroslavlancevic <lya-job@yandex.ru>
2019-08-18 20:21:13 +0300

    baseline

---
+++ local/components/tf/crm.deal.list/lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?
+$MESS["CRM_DEAL_LIST_NAME"] = "Список потребностей";
+$MESS["CRM_DEAL_LIST_DESCRIPTION"] = "Список потребностей";
+$MESS["CRM_NAME"] = "CRM";
+$MESS["CRM_DEAL_NAME"] = "Потребности";
+?>
